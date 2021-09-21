578553bd74c6a4d2477a365c771f333275791028
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-09-21 12:47:09 +0200

    Add Student Service

---
+++ src/main/java/de/neuefische/spring_request_params/service/StudentService.java
@@ -0,0 +1,48 @@
+package de.neuefische.spring_request_params.service;
+
+import de.neuefische.spring_request_params.model.Student;
+import de.neuefische.spring_request_params.repo.StudentRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StudentService {
+
+    private StudentRepo studentRepo;
+
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    public List<Student> list() {
+        return studentRepo.getStudents();
+    }
+    public Student findById(String id) {
+        Optional<Student> optionalStudent = studentRepo.findById(id);
+        if (optionalStudent.isPresent()) {
+            return optionalStudent.get();
+        }
+        throw new IllegalArgumentException("Id not found!");
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepo.add(student);
+    }
+
+    public List<Student> search(String s) {
+        List<Student> searchResultList = new ArrayList<>();
+        for (Student student: list()) {
+            if (student.getName().contains(s)){
+                searchResultList.add(student);
+            }
+        }
+
+        return searchResultList;
+    }
+
+    public void delete(String id) {
+        Student student = findById(id);
+        studentRepo.delete(student);
+    }
+}
