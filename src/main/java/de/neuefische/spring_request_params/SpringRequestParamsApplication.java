aecb647f4aa7b3358e122c7c552480b7ac316ee1
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-09-21 12:46:27 +0200

    Init Spring Project

---
+++ src/main/java/de/neuefische/spring_request_params/SpringRequestParamsApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.spring_request_params;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestParamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRequestParamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/spring_request_params/SpringRequestParamsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.spring_request_params;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRequestParamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
